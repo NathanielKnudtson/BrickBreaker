6db479fa59245789ab79def5abff8f91b2000e65
NKnudtson <nknudtso@depaul.edu>
2022-01-24 16:38:46 -0600

    Configured paddle and ball
    Set up ball and paddle scripts, added walls and set up defeat zone/trigger, made ball material bouncy

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public bool ballLaunched = false;
+    public Rigidbody2D ballRigidbody;
+    public Vector2[] startDirections;
+    public int randomNum;
+    public float ballForce;
+    public Vector3 startPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !ballLaunched)
+        {
+            randomNum = Random.Range(0, startDirections.Length);
+            ballRigidbody.AddForce(startDirections[randomNum] * ballForce, ForceMode2D.Impulse);
+            ballLaunched = true;
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DefeatZone")
+        {
+            ballRigidbody.velocity = Vector3.zero;
+            transform.position = startPosition;
+            ballLaunched = false;
+        }
+    }
+
+}
